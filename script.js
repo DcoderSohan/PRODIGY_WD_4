951f302eb2774f2270706033b4e84002e0fe49c4
DcoderSohan <dcoder0709@gmail.com>
2024-07-04 15:11:49 +0530

    creating about page

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    const phoneNumber = '1234567890'; 
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Name:%20${encodeURIComponent(name)}%0AEmail:%20${encodeURIComponent(email)}%0AMessage:%20${encodeURIComponent(phone)}%0AMessage:%20${encodeURIComponent(message)}`;
+
+    window.open(whatsappUrl, '_blank');
+});
