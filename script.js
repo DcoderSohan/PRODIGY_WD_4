8a4f6fc994781cfe3d78afbd18ae9be247039a58
DcoderSohan <dcoder0709@gmail.com>
2024-07-05 21:19:05 +0530

    creating resume page

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
     const message = document.getElementById('message').value;
+    const msg ="I'm interested !!"
 
     const phoneNumber = '1234567890'; 
     const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Name:%20${encodeURIComponent(name)}%0AEmail:%20${encodeURIComponent(email)}%0AMessage:%20${encodeURIComponent(phone)}%0AMessage:%20${encodeURIComponent(message)}`;
